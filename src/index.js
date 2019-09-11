67219bff4c6ecc12fc11ea3b9a2dc5df66e39c6a
AlexxxxK <oleksandr.kozyr@outlook.com>
2019-09-12 02:40:45 +0300

    User Prifile

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Profile from "./Profile/Profile";
+
+const user = {
+  name: "Jacques Gluke",
+  tag: "@jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+ReactDOM.render(<Profile user={user} />, document.getElementById("root"));
